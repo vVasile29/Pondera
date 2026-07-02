589945ea6937b8cc3846344b78713c1445824142
Valentin Vasile <work@vasile.digital>
2026-07-02 18:33:50 +0200

    turn PRAGMA on for correct cascading of decisions and custom metrics

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import os
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./optium.db")
@@ -13,6 +13,15 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     **engine_kwargs,
 )
+
+# Enable foreign key enforcement in SQLite so ON DELETE CASCADE works
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+
+    @event.listens_for(engine, "connect")
+    def _set_sqlite_pragma(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
